52ff598ba1e02da738ae5a83da91b7b040bc4a6a
hassan12ammar <mgamea125@gmail.com>
2022-07-03 01:32:14 +0300

    to deploy on heroku

--- main.py
+++ main.py
@@ -45,16 +45,16 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    updater.start_polling()
+    # updater.start_polling()
 
-    # updater.start_webhook(
-    #     listen = '0.0.0.0',
-    #     port = PORT,
-    #     url_path = API_KEY,
-    #     webhook_url = URL + API_KEY, )
-    # updater.bot.setWebhook(URL + API_KEY)
+    updater.start_webhook(
+        listen = '0.0.0.0',
+        port = PORT,
+        url_path = API_KEY,
+        webhook_url = URL + API_KEY, )
+    updater.bot.setWebhook(URL + API_KEY)
 
-    # updater.idle()
+    updater.idle()
 
 
 if __name__ == '__main__':
